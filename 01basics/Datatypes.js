b6335960ec9b8315dadae1f1e0c20db801ac9c4f
Rajat20011002 <145523819+Rajat20011002@users.noreply.github.com>
2023-09-20 05:43:58 +0000

    container config done

---
+++ 01basics/Datatypes.js
@@ -0,0 +1,20 @@
+"use strict;"//treat all JS code as  newer version
+//alert("hello");  we are using nodejs, not browser
+console.log(); // code readability should be high
+let name = "rajat"
+let age = 22;
+let isLoginedIn = false
+ null;
+
+// Number => 2  to the power 53
+// bigInt 
+// String=> ""
+//boolean=> true/false
+// null => standalone value
+// undefined => 
+// Symbol => unique 
+
+
+// objects  
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144533;
+let accountEmail = " rajatlakhwan77@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+ let accountState;
+
+//accountId = 2  not allowed 
+accountEmail = "hc@hv.com"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functinal scope 
+*/
