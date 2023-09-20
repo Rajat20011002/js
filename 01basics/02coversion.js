b63add6ae937a0b45088e9bd059e61c852cfb2cf
Rajat20011002 <145523819+Rajat20011002@users.noreply.github.com>
2023-09-20 07:34:14 +0000

    conversion

---
+++ 01basics/02coversion.js
@@ -0,0 +1,25 @@
+// let score =undefined;
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let  valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log( valueInNumber);
+// let isloggedIn = "rajat";
+// let booleanIsloggedIn =Boolean(isloggedIn)
+
+// console.log(booleanIsloggedIn); 
+// 1 => true
+// 0=> false   ""=> false   . "rajat"=> true
+
+
+/*"33" => 33
+"33abc"=> NaN
+true =>1;
+false =>0;
+
+*/
+
+let someNumber = 446;
+let StringNumber = String(someNumber)
+console.log( typeof StringNumber);
